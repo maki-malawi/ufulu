b8824c6f88a3590799567350a383aa54cbb1b7c0
Martin Suleman <sulemartin87@gmail.com>
2021-05-18 09:49:31 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    infinite: true,
+  });
+  let burger = document.querySelector('.burger');
+  let navbar = document.querySelector('.navbar-menu');
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    navbar.classList.toggle('is-active');
+  });
+});
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
